6e12870d107104dbe1a8cdb50421e69fdd128538
Joseph Rex <joerexko@gmail.com>
2017-11-07 11:28:56 -0600

    Add basic animation for comments toggling

--- static/js/src/main.js
+++ static/js/src/main.js
@@ -12,6 +12,7 @@ function ready(cb) {
     : cb();
 }
 ready(function(){
+  var disqusContainer = document.getElementById('disqus_thread');
   // Progressive reading indicator
   var indicator = document.querySelector('.scroll-progress');
   if(indicator){
@@ -26,4 +27,34 @@ ready(function(){
   }
   // Highlight with Prism
   Prism.highlightAll();
+  // Toggle Disqus comments
+  var commentTrigger = document.getElementById('toggleComments');
+  if(commentTrigger){
+    var disqus_config = function() {
+      this.page.url = document.querySelector('[rel="canonical"]').href;
+      this.page.identifier = document.querySelector('[property="og:title"]')
+      .content.replace(' ', '-');
+    };
+    commentTrigger.addEventListener('click', function() {
+      var ch = disqusContainer.clientHeight;
+      document.querySelector('.comments').classList.add('comments__animate');
+      // document.querySelector('.comments__animate').style
+      // .setProperty('--origin', ch > 0 ? 'bottom' : 'top')
+      disqusContainer.classList.toggle('u-ghost');
+      setTimeout(function() {
+        document.querySelector('.comments').classList.remove('comments__animate');
+      }, 2000);
+      disqusComments();
+    });
+  }
 });
+
+function disqusComments() {
+  var disqus_shortname = document.querySelector('[data-disqus]').dataset.disqus;
+  var dsq = document.createElement('script');
+  dsq.src = '//' + disqus_shortname + '.disqus.com/embed.js';
+  if(typeof(DISQUS) === 'undefined') {
+    (document.getElementsByTagName('head')[0] || document.getElementsByTagName('body')[0])
+    .appendChild(dsq);
+  }
+}
